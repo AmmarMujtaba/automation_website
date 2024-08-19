86e3bfbabbad595282d7e1323584b104e7c24079
AmmarMujtaba <ammarmujtaba70@outlook.com>
2024-08-20 00:14:59 +0500

    modified cors in server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors)
+app.use(cors({
+    origin:'*',
+    methods:['GET','POST'],
+    allowedHeaders: ['Content-Type','Authorization']
+}))
 
 app.get('/on',(req,res) => {
     res.send('Server running AlhamdulILLAH')
