354adcb2d5e9b92a909aacb4dfb0965a6fc9c405
AmmarMujtaba <ammarmujtaba70@outlook.com>
2024-08-20 00:20:51 +0500

    modified client side response handling

--- src/client.js
+++ src/client.js
@@ -2,6 +2,9 @@ export default function fanBtnClicked(){
     console.log('inside handler function')
     fetch('https://as-server-orpin.vercel.app/on')
     .then((response) => {
-        console.log('response: ',response)
+        return response.text
+    })
+    .then((text) => {
+        console.log('response: ',text)
     })
 }
