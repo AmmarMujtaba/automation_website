c84bc804d927665b9bf58e77c3d48d2b4801e98b
AmmarMujtaba <ammarmujtaba70@outlook.com>
2024-08-19 12:18:08 +0500

    commit 4

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 export default function fanBtnClicked(){
     console.log('inside handler function')
-    fetch('http://39.56.14.56:5555/off')
+    fetch('https://39.56.14.56:5555/off')
     .then((response) => {
         console.log('response: ',response)
     })
