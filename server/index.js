607415508b961c73bda4d82412f95b0e09e391f6
AmmarMujtaba <ammarmujtaba70@outlook.com>
2024-08-28 11:46:56 +0500

    added websockets + improvements5

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import fetch from 'node-fetch'
 import {MongoClient} from 'mongodb'
-const ws = require('ws')
-import WebSocket from ws
-const wsServer = new WebSocket.WebSocketServer({port:5555})
+// const ws = require('ws')
+// import WebSocket from ws
+// const wsServer = new WebSocket.WebSocketServer({port:5555})
 
 const app = express()
 
@@ -15,19 +15,19 @@ const mongoClient = new MongoClient(dbUri)
 await mongoClient.connect()
 const collection = mongoClient.db('myDB').collection('cardiology')
 
-const client = null
-wsServer.on('connection',(ws) => {
-    ws.send('s=> client connected')
-    client = ws
-    ws.on('message',(response) => {
-        const text = new TextDecoder().decode(response)
-        ws.send('S=> message: ',text)
-    })
-    ws.on('close',(ws) => {
-        console.log('client disconnected')
-        client = null
-    })
-})
+// const client = null
+// wsServer.on('connection',(ws) => {
+//     ws.send('s=> client connected')
+//     client = ws
+//     ws.on('message',(response) => {
+//         const text = new TextDecoder().decode(response)
+//         ws.send('S=> message: ',text)
+//     })
+//     ws.on('close',(ws) => {
+//         console.log('client disconnected')
+//         client = null
+//     })
+// })
 
 app.get('/example',async (req,res) => {
     //find ip from database
@@ -50,11 +50,11 @@ app.get('/on',async (req,res) => {
     res.send(text)
     
     //ex
-    if(client != null){
-        if(client.readyState == WebSocket.OPEN){
-            client.send("s=> ",text)
-        }
-    }    
+    // if(client != null){
+    //     if(client.readyState == WebSocket.OPEN){
+    //         client.send("s=> ",text)
+    //     }
+    // }    
 })
 app.get('/off',async (req,res) => {
     //find ip from database
